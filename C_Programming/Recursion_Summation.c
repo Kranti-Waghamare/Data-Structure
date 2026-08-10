7e283e4a4f19c3f1c578e91f94904b2a0b15b919
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-10 10:23:05 +0530

    Summation by using Recursion

---
+++ C_Programming/Recursion_Summation.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int Summation(int iNo)
+{
+    static int iSum = 0;
+    static int i = 1;
+    
+    if(i <= iNo)
+    {
+        iSum = iSum + i;
+        i++;
+
+        Summation(iNo);
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    iRet = Summation(iValue);
+
+    printf("Summation is : %d\n", iRet);
+
+    return 0;
+}
