bd0e481fdddbb019a66adff4e9a73d7eaceb6e60
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-10 10:06:15 +0530

    Used static storage class but Issue due to Reinitialization

---
+++ C_Programming/Recursion7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+//Recursion
+void Display()
+{
+    static int i = 0;
+
+    i = 1;
+
+    if(i <= 4)
+    {
+        printf("Jay Ganesh...%d\n", i);
+        i++;
+        Display();
+    }
+}
+
+int main()
+{
+    Display();
+   
+    return 0;
+}
